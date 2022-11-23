1f24c6caaec19af3de22bf5da0291f0785e06df2
Alexey Mironov <al.g.mironov@yandex.ru>
2022-11-23 21:32:02 +0300

    Bot is now starting

--- src/main/java/com/github/algmironov/glucose_data_measurement_service/configuration/TelegramBotConfiguration.java
+++ src/main/java/com/github/algmironov/glucose_data_measurement_service/configuration/TelegramBotConfiguration.java
@@ -1,2 +1,22 @@
-package com.github.algmironov.glucose_data_measurement_service.configuration;public class TelegramBotConfiguration {
+package com.github.algmironov.glucose_data_measurement_service.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.DeleteMyCommands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+
 }
